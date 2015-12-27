a242a421281eaa8aeb5e96cd43289080b352deb7
jdb <git@aliantux.org>
2015-12-27 17:11:41 +0100

    adj html

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 Measuring the actual capacity of a battery by an controlled discharge
+
+Contexte :
+==========
+C'est la seule source d'énergie disponible à bord pendant tout un vol qui peut durer de 5 minutes à 10 heures avec des vols habituels de 3 à 5 heures
+
+Il est donc important de savoir sur quelle quantité d'énergie on peut compter.
+
+Tous les instruments de base du pilotage fonctionnent avec des pressions d'air ( statique, dynamique   …), la batterie n'est pas « vitale » pour assurer le pilotage mais est devenue indispensable en terme de sécurité.
+La batterie a été introduite dans un premier temps afin de faire fonctionner la radio, puis les variomètres électroniques sont apparus puis les calculateurs de vols avec GPS, et enfin les systèmes anti-collision (Flarm).
+Certain planeurs sont même aujourd'hui équipés de transpondeurs et de feux à éclats afin d'être vus plus tôt par les autres  pilotes.
+
+Compte tenu de tout ces équipements, la consommation moyenne lors d'un vol est comprise entre 0,5 et 1 A ( 2 à 3 A de plus lors d'un message radio.)
+
+C'est pourquoi on dispose à bord d'une ou deux batteries ( voir 3) de 7 ou 14 A/H, elles ont amovibles et rechargées systématiquement après chaque vol dans un local dédié à cet usage.
+
+Actuellement les batteries plomb étanches sont les plus courantes.
+Lors des visites annuelles pendant la période hivernale, j'ai conçu ce testeur afin d'avoir l'image la plus juste de l'état de la batterie et de déceler à coup sur les batteries défectueuses.
+
+Principe :
+=========
+Le principe est de prendre une batterie chargée et de la décharger au 1/10 de sa capacité (correspondant grosso-modo aux conditions de vol) pendant une durée max de 10 heures avec arrêt automatique du test si la tension batterie descend au dessous de 11 V.
+Les mesures et un graph de suivi sont effectuées toutes les minutes via une tache cron, le paramétrage, la commande du test et la visualisation sont réalisés via une interface web et les données stockées sur une base rrd
+
+Hardware :
+==========
+Le hardware est basé sur le populaire Raspberry-PI , à l'origine un modèle B mais est tout à fait transposable sur un B+, un 2 et même testé sur un PI-0.
+J'ai créé une platine complémentaire comportant des relais de commutation de la batterie et des résistances de décharge, un convertisseur analogique numérique, des systèmes de protection, avec en option une horloge RTC, un mini écran 2,5" afin de rendre l'appareil autonome.
+
+Software :
+==========
+L'applicatif utilise une page web php qui renseigne un fichier d'échange et assure la visualisation instantanée de l'avancement du test  et  un programme en python qui assure le contrôle, les mesures et le stockage des données
---
+++ html/testbat-0.1.php
@@ -0,0 +1,258 @@
+<?php /*	========= Testbat-x.y.php =======================
+			Test des batteries en liaison avec testbat-x.y.py
+			par passage des paramètres dans  testbat.param
+			(c)aliantux.org jdb 2015-2016
+			=================================================
+*/
+require "params.inc.php";
+require "toppage.inc.php";
+date_default_timezone_set ("Europe/Paris");
+//============ A valider après débug ========
+//ini_set('display_errors', 1);
+error_reporting(E_ALL);
+//error_reporting(0);
+$row=array();
+echo "<body>\n";
+//date_default_timezone_set("Europe/Paris");echo "Europe/Paris:".time(); 
+// ========== récupération des paramètres ======
+echo "<p class=inf1>\n";
+$f = fopen($batPath."testbat.param", "r");
+	$immat=fparam(fgets($f));print $immat.", ";
+	$batNum=fparam(fgets($f));print $batNum.", ";
+	$batCap=fparam(fgets($f));print $batCap.", ";
+	$testSt=fparam(fgets($f));print $testSt.", ";
+	$testFin=fparam(fgets($f));print $testFin.", ";
+	$testBrk=fparam(fgets($f));print $testBrk.", ";
+	$relUdem=fparam(fgets($f));print $relUdem.", ";
+	$rel1st=fparam(fgets($f));print $rel1st.", ";
+	$rel2st=fparam(fgets($f));print $rel2st.", ";
+	$rel3st=fparam(fgets($f));print $rel3st.", ";
+	$rel4st=fparam(fgets($f));print $rel4st.", ";
+	$tsDeb=fparam(fgets($f));print $tsDeb.", ";
+	$tsFin=fparam(fgets($f));print $tsFin."\n";
+fclose($f);
+echo "</p>\n";
+
+// ============== récup des paramètres ============
+$kimmat=$immat;
+$kbn=$batNum; 
+$kbc=$batCap;
+$ktst=$testSt;
+$ktf=$testFin;
+$kbrk=$testBrk;
+$krud=$relUdem;
+if ($tsDeb=="0")$dift="0";
+else $dift=time()-$tsDeb;
+/*echo time()."<br>";
+echo $dift."<br>";
+echo date('H:i:s   Y-m-d',time())."<br>";
+echo date('H:i:s   Y-m-d',$tsDeb)."<br>";
+echo strftime('%T',$dift)."<br>";
+echo gmstrftime('%T',$dift)."<br>";*/
+
+// ========== Images par défaut ======================
+$ledbrk="img/led-rd.png";
+$bat="img/bat-gy.png";//$led="img/led-gy.png";
+if ($rel2st=="1") $lr2="img/lOn.png"; else $lr2="img/lOff.png"; 
+if ($rel3st=="1") $lr3="img/lOn.png"; else $lr3="img/lOff.png"; 
+if ($rel4st=="1") $lr4="img/lOn.png"; else $lr4="img/lOff.png"; 
+
+// ========== récupération du status des relais ======
+if ($rel1st==0 and $rel2st==0){
+	$bat="img/bat-gy.png";//$led="img/led-gy.png";
+	}
+elseif($rel1st==1 and $rel2st==0){
+	$bat="img/bat-gn.png";//$led="img/led-gy.png";
+	}
+elseif($rel1st==1 and $rel2st==1){
+	$bat="img/bat-or.png";//$led="img/led-rd.png";
+	}
+
+// ============== test des actions POST =====================
+if (isset($_POST['cmd'])){
+	//echo "<br>".$_POST['cmd']."  ".$_POST['immat']."  ".$_POST['Nb']."  ".$_POST['Cb']."<br>";
+	if ($_POST['cmd']=='ENREGISTRER'){
+		$kimmat=$_POST['immat']; $kbn=$_POST['Nb']; $kbc=$_POST['Cb'];
+		$krud=$_POST['Bat'];
+		wrtParam($batPath,$kimmat,$kbn,$kbc,$ktst,$ktf,$kbrk,$krud,$rel1st,$rel2st,$rel3st,$rel4st,$tsDeb,$tsFin);
+	}
+	elseif ($_POST['cmd']=='Deb_Test') {
+		if ($kbrk=='1'){
+			//echo" BRK=1";
+			$strMsg="Impossible: BREAK en cours";
+			echo '<script type="text/javascript"> alert("Test : '.$strMsg.'")</script>';
+			$krud="0";$ktst="0";
+		}
+		elseif ($ktst=="1") {
+			$strMsg="Test déjà en cours!";
+            echo '<script type="text/javascript"> alert("Test : '.$strMsg.'")</script>';
+		}
+		elseif ($ktf=="1") {
+			$strMsg="Faire une RAZ Test au préalable";
+            echo '<script type="text/javascript"> alert("Test : '.$strMsg.'")</script>';
+		}
+		else {
+			//$date_debut = new DateTime(); c'est python qui mettra le timestamp de début
+			//echo "date_debut". $date_debut->format('%h:%i:%s')."<br>";
+			$ktst="1";$ktf="0";
+			$kimmat=$_POST['immat']; $kbn=$_POST['Nb']; $kbc=$_POST['Cb'];
+			$krud="1";$tsFin="0";
+			wrtParam($batPath,$kimmat,$kbn,$kbc,$ktst,$ktf,$kbrk,$krud,$rel1st,$rel2st,$rel3st,$rel4st,$tsDeb,$tsFin);
+		}
+	}
+	elseif ($_POST['cmd']=='Fin_Test') {
+		//$tsFin=time(); c'est python qui mettra le timestamp de fin
+		$ktf=1;// c'est python qui mettra à zero $ktst
+		wrtParam($batPath,$kimmat,$kbn,$kbc,$ktst,$ktf,$kbrk,$krud,$rel1st,$rel2st,$rel3st,$rel4st,$tsDeb,$tsFin);
+		}
+	elseif ($_POST['cmd']=='Raz_Brk') {
+		$kbrk="0";
+		wrtParam($batPath,$kimmat,$kbn,$kbc,$ktst,$ktf,$kbrk,$krud,$rel1st,$rel2st,$rel3st,$rel4st,$tsDeb,$tsFin);
+		}
+	elseif ($_POST['cmd']=='Raz_Test') {
+		$tsDeb='0';$tsFin="0";$ktf="0";$ktst="0";
+		wrtParam($batPath,$kimmat,$kbn,$kbc,$ktst,$ktf,$kbrk,$krud,$rel1st,$rel2st,$rel3st,$rel4st,$tsDeb,$tsFin);
+		}
+	}
+//---------------------------------------
+
+/* liste des planeurs*/
+$f = fopen($batPath."planeur.lst", "r");
+$n=0;
+while(!feof($f)) {
+    $n=++$n ;
+    $lrow[$n]=rtrim(fgets($f));
+	//echo $n." ".$lrow[$n]."  ".strlen($lrow[$n])."<br>\n";
+	}
+?>
+<div id="page">
+ <div id="header">
+ <h1>Graphique test de décharge batterie </h1>
+ </div>
+ <div id="body">
+	<form method='POST' action='testbat.php'>
+	<table border="0">
+      <tr>
+	    <td valign='top'>
+			<a href="http://www.facebook.com/PlaneurChalles" target="_blank">
+			<img src="img/brand-planeurschalles.png" border="0"></a>
+		</td>
+	    <td valign='top' align='center'>
+			<br><select name='immat'>
+			<?php
+			// remplissage de la combo list
+			for ($i=1; $i <= $n; $i++){
+				//$id=$lrow[$i]['id_planneur'];
+            	echo "<option"; //.$lrow[$i];
+				if ($kimmat==$lrow[$i]){
+					$immatSelected=$lrow[$i];
+					echo " selected";
+				}
+				echo ">".$lrow[$i]."</option>\n";
+				//echo $kimmat."  ".$lrow[$i]." immatselected:".$immatSelected."\n";
+			}
+			echo "</select>\n<br><br><hr>\n";
+   	    echo "</td><td rowspan ='3' align='center'>\n";
+			echo "<img src='img/ubat.png' alt='Ubat.png'>\n";
+			//echo "<br>kbrk:".$stbrk." l:".strlen($stbrk).", ktst:".$ktest." l:".strlen($ktest)."<br>\n";
+			if ($kbrk=='1'){
+				echo "<h3 class='brk' >Break en cours !</h3>\n";
+				}
+			if ($ktst=='1' and $tsDeb !="0"){
+				echo "<h3 class='tst' >Test en cours, durée: ".gmstrftime('%T',$dift)."</h3>\n";
+				}
+			if ($ktf=="1" and $ktst=="0") {
+				echo "<h3 class='tst' >Test Terminé, durée: ".gmstrftime('%T',$tsFin-$tsDeb)."</h3>\n";
+			}
+		echo "</td>\n";
+	echo "</tr><tr>\n";
+		echo "<td valign='top' align='left'>\n";
+			echo "<img src='".$bat."' alt='bat'>&nbsp;&nbsp;<br>\n";
+			echo "On:<input type='radio' name='Bat' value='1'";
+            if ($krud=="1") echo " checked ><br>\n"; else echo " ><br>\n";
+			echo "Off:<input type='radio' name='Bat' value='0'";
+            if ($krud=="0") echo " checked >\n"; else echo " >\n";
+			echo "<br><br>R2:<img src='".$lr2."' alt='r2'>\n";
+			echo "<br>R3:<img src='".$lr3."' alt='r3'><br>\n";
+			if ($rel4st=="1"){
+				echo "R4:<img src='".$lr4."' alt='r4'>\n";
+			}
+		echo "</td>\n";
+		echo "<td>\n";
+			echo "Bat.Numéro:<br>\n";
+			echo "1:<input type='radio' name='Nb' value='1'";
+			if ($kbn=="1") echo " checked > &nbsp;\n"; else echo " > &nbsp;\n";
+			echo "2:<input type='radio' name='Nb' value='2'";
+			if ($kbn=="2") echo " checked > <br>\n"; else echo " > <br>\n";
+			echo "3:<input type='radio' name='Nb' value='3'";
+			if ($kbn=="3") echo " checked > &nbsp;\n"; else echo " > &nbsp;\n";
+			echo "4:<input type='radio' name='Nb' value='4'";
+			if ($kbn=="4") echo " checked > \n"; else echo " > \n";
+			echo "<br><br>\n";
+			echo "Bat.Capacité:<br>\n";
+			echo "7:<input type='radio' name='Cb' value='7'";
+			if ($kbc=="7") echo " checked > &nbsp;\n"; else echo " > &nbsp;\n";
+			echo "14:<input type='radio' name='Cb' value='14'";
+			if ($kbc=="14") echo " checked >\n"; else echo " >\n";
+			echo "<input type='hidden'  name='Ps' value='".$immatSelected."'><br>\n";
+			echo "<input type='hidden'  name='Ton' value='".$ktst."'><br>\n";
+			?>
+			<br>
+			<input type='submit' value='ENREGISTRER' name='cmd'>&nbsp;&nbsp;
+	        <br><br><hr>
+		</td>
+      </tr><tr>
+		<td align='left' valign='top'>
+			<?php if ($kbrk=="1"){
+				echo "<br><br>&nbsp;<img src='".$ledbrk."' alt='break'>\n";
+				echo "<br><b><font color='red'>&nbsp;Break!</font></b><br><br>\n";
+            	echo "<input type='submit' value='Raz_Brk' name='cmd'>&nbsp;&nbsp;<br>&nbsp;\n";
+			}?>
+		</td>
+		<td align='center'>
+			<input type='submit' value='Deb_Test' name='cmd'>
+	        <br><br>
+			<input type='submit' value='Fin_Test' name='cmd'>
+	        <br><br>
+			<input type='submit' value='Raz_Test' name='cmd'>
+		</td>
+	</tr>
+  </table>
+  </form>
+  <hr>
+  <?php
+echo "<h3 class='inf' >Ne pas oublier de remettre la batterie en charge après le test</h3>\n";
+echo "Derniere mise &agrave; jour le ".date("d-m-Y")." &agrave; ". date ("H\:i:s")."<br>\n";
+echo "Le test s'arrêtera si la tension devient inférieure à 11V ou si la durée de décharge atteint 10 heures\n";
+echo "</div></div>\n"; //fin de div page et div body
+require "btmpage.inc.php";
+// ************fonctions perso*************
+// ================= Fonctions =============================
+function fparam($strLine){
+	# extraction du paramètre situé entre le "=" et le ";"
+	$pos1 = strpos($strLine,"=");$pos2=strpos($strLine,";");
+    $retpar = substr($strLine,$pos1+1,$pos2-$pos1-1);
+	return $retpar;
+}
+//========== ecriture des parametres
+function wrtParam($bpath,$wimmat,$wnb,$wnc,$wtst,$wtf,$wbrk,$wrud,$wr1st,$wr2st,$wr3st,$wr4st,$wtsDeb,$wtsFin){
+	$handle = fopen($bpath."testbat.param", "w");
+	fwrite($handle, "immat=".$wimmat.";\n");
+	fwrite($handle, "batNum=".$wnb.";\n");
+	fwrite($handle, "batCap=".$wnc.";\n");
+	fwrite($handle, "testSt=".$wtst.";\n");
+	fwrite($handle, "testFin=".$wtf.";\n");
+	fwrite($handle, "testBrk=".$wbrk.";\n");
+	fwrite($handle, "relUdem=".$wrud.";\n");
+	fwrite($handle, "rel1st=".$wr1st.";\n");
+	fwrite($handle, "rel2st=".$wr2st.";\n");
+	fwrite($handle, "rel3st=".$wr3st.";\n");
+	fwrite($handle, "rel4st=".$wr4st.";\n");
+	fwrite($handle, "tsDeb=".$wtsDeb.";\n");
+	fwrite($handle, "tsFin=".$wtsFin.";\n");
+	fclose($handle);
+	echo "<p class=inf2>\n";
+		echo "Enregistrement params: Ok\n";
+	echo "</p>\n";
+}
+?>
